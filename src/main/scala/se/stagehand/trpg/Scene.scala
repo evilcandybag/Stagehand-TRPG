bca934893326ba29df7bc133fecab598acaac167
Peter Holm <peter@insella.se>
2014-01-05 15:58:02 +0100

    support adding and removing effects to a Scene.scala

--- src/main/scala/se/stagehand/trpg/Scene.scala
+++ src/main/scala/se/stagehand/trpg/Scene.scala
@@ -4,12 +4,21 @@ import se.stagehand.lib.scripting._
 import scala.xml._
 import scala.swing._
 import scala.swing.event.Key
+import se.stagehand.lib.Log
 
-class Scene extends ScriptComponent with Input {
+class Scene(id:Int) extends ScriptComponent(id) with Input {
+  private val log = Log.getLog(this.getClass())
+  
+  def this() = this(ID.unique)
+  
   override def componentName = "Scene"
   
-  var effects: List[Effect] = List()
-    
+  private var _effects: List[Effect] = List()
+  
+  def add(e:Effect) = _effects = e :: _effects
+  def remove(e:Effect) = _effects = _effects diff List(e)
+  def effects = _effects
+  
   def act:Unit = {
     loop {
       react {
@@ -19,12 +28,17 @@ class Scene extends ScriptComponent with Input {
   }
     
   def executeInstructions(params: Any*) {
-    effects.foreach( x => 
+    _effects.foreach( x => 
       x.trigger
     )
   }
   override def readInstructions(in: Node) {
     super.readInstructions(in)
+    val fxXML = (in \ "effects") \ "id"
+    log.debug(fxXML.toString)
+    val fx = fxXML.map(x => ID.fetch[Effect](x.text.toInt))
+    
+    _effects = fx.toList
   }
 
   override def generateInstructions = {
@@ -35,6 +49,6 @@ class Scene extends ScriptComponent with Input {
   }
   
   private def effectsXML = {
-    <effects>{effects.map(_.idXML)}</effects>
+    <effects>{_effects.map(_.idXML)}</effects>
   }
 }
