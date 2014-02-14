ff25eb20ccf3c13690b2704d7d16c24e3940ab77
Peter Holm <peter@insella.se>
2014-02-14 12:41:57 +0100

    Added sourceArgs injection

--- src/main/scala/se/stagehand/trpg/Scene.scala
+++ src/main/scala/se/stagehand/trpg/Scene.scala
@@ -5,18 +5,28 @@ import scala.xml._
 import scala.swing._
 import scala.swing.event.Key
 import se.stagehand.lib.Log
+import se.stagehand.lib.scripting.network.Directives
 
 class Scene(id:Int) extends ScriptComponent(id) with Input {
-  private val log = Log.getLog(this.getClass())
+  import Directives._
   
+  private val log = Log.getLog(this.getClass())
   def this() = this(ID.unique)
   
   override def componentName = "Scene"
-  
+  val description = "A collection of scenes that can be activated at the same time."
+    
+    
   private var _effects: List[Effect] = List()
   
-  def add(e:Effect) = _effects = e :: _effects
-  def remove(e:Effect) = _effects = _effects diff List(e)
+  def add(e:Effect) = {
+    e.addArg(Persistence.ARG, Persistence.CLEAR)
+    _effects = e :: _effects
+  }
+  def remove(e:Effect) = {
+    e.removeArg(Persistence.ARG)
+    _effects = _effects diff List(e)
+  }
   def effects = _effects
     
   def executeInstructions(params: Any*) {
